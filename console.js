f8481d7ba5fbb4b003c46c9f5c50fb7871fd04a4
Nasin Ayenul Akash <79800839+17-35060-2Akash@users.noreply.github.com>
2023-01-31 04:40:49 +0600

    Update console.js

--- console.js
+++ console.js
@@ -1,12 +1,12 @@
 //heythis is for experiment
 
 console.log("hello world");
-console.log("hello world");
+
 //
 
 function main(){
   const arr=[1,1,1,1];
 return null;
-}//this is not working
+
 
 console.log(main);
